723fa096cfdd4190b28648d120cef9db2561cb6e
藍 <50108258+kwaa@users.noreply.github.com>
2022-01-15 23:54:54 +0800

    ✨ feed icon & category

--- src/routes/atom.xml.ts
+++ src/routes/atom.xml.ts
@@ -1,39 +1,50 @@
 import type { EndpointOutput } from '@sveltejs/kit'
 import { site } from '$lib/config/site'
 import { feed } from '$lib/config/misc'
+import { icons } from '$lib/config/icons'
 import { genPosts } from '$lib/utils/posts'
+import { genTags } from '$lib/utils/tags'
 
-const render = async (): Promise<string> => `<?xml version='1.0' encoding='utf-8'?>
+const render = async (): Promise<string> => {
+  const posts = Object.entries(await genPosts())
+    .flatMap(([key, value]) => (+key > 0 ? value : []))
+    .sort((a, b) => (b.date ?? '1989-06-04').localeCompare(a.date ?? '1989-06-04'))
+    .filter((_, index) => feed.limit === 0 || index < feed.limit)
+  const tags = genTags(posts)
+  return `<?xml version='1.0' encoding='utf-8'?>
 <feed xmlns="http://www.w3.org/2005/Atom">
+  <id>${site.url}/</id>
   <title><![CDATA[${site.title}]]></title>
   ${site.subtitle ? `<subtitle><![CDATA[${site.subtitle}]]></subtitle>` : ''}
+  ${icons.favicon ? `<icon>${icons.favicon.src}</icon>` : ''}
   <link href="${site.url}" />
-  <link href="${site.url}/atom.xml" rel="self" />
+  <link href="${site.url}/atom.xml" rel="self" type="application/atom+xml" />
   ${feed.hub !== false ? `<link href="${feed.hub}" rel="hub"/>` : ''}
   <updated>${new Date().toJSON()}</updated>
   <author>
     <name><![CDATA[${site.author.name}]]></name>
   </author>
-  <id>${site.url}/</id>
-  ${Object.entries(await genPosts())
-    .flatMap(([key, value]) => (+key > 0 ? value : []))
-    .map((post, index) => {
-      if (feed.limit === 0 || index < feed.limit)
-        return `<entry>
+  ${Object.keys(tags)
+    .map(tag => `<category term="${tag}" scheme="${site.url}?tags=${tag}" />`)
+    .join('\n')}
+  ${posts
+    .map(
+      post => `<entry>
     <title type="html"><![CDATA[${post.title}]]></title>
     <link href="${site.url + post.path}" />
     <id>${site.url + post.path}</id>
     <published>${new Date((post.date ??= '2021-11-01')).toJSON()}</published>
-    <updated>${new Date(post.lastmod ?? post.date).toJSON()}</updated>${
-          post.descr ? `\n    <summary type="html"><![CDATA[${post.descr.toString()}]]></summary>` : ''
-        }
+    <updated>${new Date(post.lastmod ?? post.date).toJSON()}</updated>
+    ${post.descr ? `<summary type="html"><![CDATA[${post.descr.toString()}]]></summary>` : ''}
     <content type="html">
       <![CDATA[${post.html}]]>
     </content>
+    ${post.tags ? post.tags.map(tag => `<category term="${tag}" scheme="${site.url}?tags=${tag}" />`).join('\n') : ''}
   </entry>`
-    })
-    .join('\n  ')}
+    )
+    .join('\n')}
 </feed>`
+}
 
 export const get = async (): Promise<EndpointOutput> => ({
   headers: {
