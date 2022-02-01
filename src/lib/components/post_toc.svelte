9669bd2bd7b19af1d3f350eaf4dc9224d7fc9d67
藍 <50108258+kwaa@users.noreply.github.com>
2022-02-01 23:25:22 +0800

    feat(toc): 💄 lunar new year update
    use daisyui menu class
    browserside heading highlight

--- src/lib/components/post_toc.svelte
+++ src/lib/components/post_toc.svelte
@@ -3,13 +3,61 @@
 </script>
 
 <script lang="ts">
+  import { onMount, onDestroy } from 'svelte'
+  import { browser } from '$app/env'
   import Tree from '$lib/components/post_toc_tree.svelte'
   export let toc: Urara.PostToc[]
+
+  let intersecting: string[] = []
+  let intersectingArticle: boolean = true
+  let bordered: string[] = []
+
+  onMount(() => {
+    if (browser) {
+      if (window.screen.availWidth >= 1280) {
+        const headingsObserver = new IntersectionObserver(
+          headings =>
+            headings.forEach(heading =>
+              heading.isIntersecting
+                ? intersecting.push(heading.target.id)
+                : (intersecting = intersecting.filter(h => h !== heading.target.id))
+            ),
+          { rootMargin: '-64px 0px 0px 0px' }
+        )
+        const articleObserver = new IntersectionObserver(article => (intersectingArticle = article[0].isIntersecting))
+        Array.from(document.querySelectorAll('main h2, main h3, main h4, main h5, main h6')).forEach(element =>
+          headingsObserver.observe(element)
+        )
+        articleObserver.observe(document.getElementsByTagName('main')[0])
+      }
+    }
+  })
+
+  onDestroy(() => {
+    if (browser) {
+      // @ts-ignore: Cannot find name 'headingsObserver'
+      if (typeof headingsObserver !== 'undefined') headingsObserver.disconnect()
+      // @ts-ignore: Cannot find name 'articleObserver'
+      if (typeof headingsObserver !== 'undefined') articleObserver.disconnect()
+    }
+  })
+
+  $: if (intersecting.length > 0) bordered = intersecting
+  $: if (intersectingArticle === false) bordered = []
+  $: if (browser && bordered)
+    toc.forEach(heading => {
+      if (bordered.includes(heading.slug)) {
+        document.getElementById(`toc-link-${heading.slug}`)?.classList.add('!border-accent')
+        document.getElementById(`toc-item-${heading.slug}`)?.classList.add('bordered')
+      } else {
+        document.getElementById(`toc-link-${heading.slug}`)?.classList.remove('!border-accent')
+        document.getElementById(`toc-item-${heading.slug}`)?.classList.remove('bordered')
+      }
+    })
 </script>
 
-<nav aria-label="TableOfContent" class="sticky top-16 py-8">
+<nav id="post-toc" aria-label="TableOfContent" class="sticky top-16 py-8">
   <Tree
-    count={0}
     toc={toc.reduce(
       (acc, heading) => {
         let parent = acc
--- src/lib/components/post_toc_tree.svelte
+++ src/lib/components/post_toc_tree.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
   export let toc: Urara.PostToc
-  export let count: number
   const { title, slug, children } = toc
-  count += 1
 </script>
 
 {#if title}
   <a
     on:click={() => document.getElementById(slug).scrollIntoView({ behavior: 'smooth' })}
-    class="transition-all hover:text-accent"
+    id={`toc-link-${slug}`}
+    class="transition-all hover:!border-primary"
     href={'javascript:void(0)'}>
     {title}
   </a>
 {/if}
-{#if children}
-  <ul class:pl-4={count > 1}>
+{#if children.length > 0}
+  <ul id={`toc-list-${slug ?? 'root'}`} class="menu">
     {#each children as child}
-      <li>
-        <svelte:self {count} toc={child} />
+      <li id={`toc-item-${child.slug}`} class="hover-bordered">
+        <svelte:self toc={child} />
       </li>
     {/each}
   </ul>
