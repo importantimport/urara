eb2fd7d6cf3c9e8c4889e886efcbc8fa8c4cd3fd
藍 <50108258+kwaa@users.noreply.github.com>
2022-02-07 17:50:07 +0800

    fix(layout): 🐛 use optional chaining

--- src/lib/components/layout_post.svelte
+++ src/lib/components/layout_post.svelte
@@ -45,7 +45,7 @@
 <Flex {title} {date} {lastmod} {priority} {tags} {cover} {descr} {path}>
   <div slot="left" class="h-full" />
   <div slot="right" class="h-full">
-    {#if toc.length >= 1}
+    {#if toc?.length >= 1}
       <Toc {toc} />
     {/if}
   </div>
