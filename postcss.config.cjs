c5ec91c6a1c894a0598d5ec70331bfa9f90a857c
藍 <50108258+kwaa@users.noreply.github.com>
2022-03-10 14:59:08 +0800

    perf(cssnano): 🔧 production use only

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,5 +4,18 @@ const tailwindcss = require('tailwindcss')
 const cssnano = require('cssnano')
 
 module.exports = {
-  plugins: [postcssImport(), tailwindcss(), postcssPresetEnv(), cssnano()]
+  plugins: [
+    postcssImport,
+    tailwindcss,
+    postcssPresetEnv,
+    ...(process.env.NODE_ENV === 'production'
+      ? [cssnano({
+        preset: [
+          'default',
+          { discardComments: { removeAll: true } }
+        ]
+      })]
+      : []
+    )
+  ]
 }
