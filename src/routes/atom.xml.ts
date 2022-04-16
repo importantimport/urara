ab5e4bff4e03a066f00ac5e839def1a9f8005928
kwaa <50108258+kwaa@users.noreply.github.com>
2022-04-16 21:06:09 +0800

    refactor(atom): ♻️ optimize indent

--- src/routes/atom.xml.ts
+++ src/routes/atom.xml.ts
@@ -9,40 +9,36 @@ const render = async (
 ): Promise<string> => `<?xml version='1.0' encoding='utf-8'?>
 <feed xmlns="http://www.w3.org/2005/Atom">
   <id>${site.protocol + site.domain}/</id>
-  <title><![CDATA[${site.title}]]></title>
-  ${site.subtitle ? `<subtitle><![CDATA[${site.subtitle}]]></subtitle>` : ''}
-  ${icon.favicon ? `<icon>${icon.favicon.src}</icon>` : ''}
+  <title><![CDATA[${site.title}]]></title>${site.subtitle ? `\n  <subtitle><![CDATA[${site.subtitle}]]></subtitle>` : ''}${
+  icon.favicon ? `\n  <icon>${icon.favicon.src}</icon>` : ''
+}
   <link href="${site.protocol + site.domain}" />
-  <link href="${site.protocol + site.domain}/atom.xml" rel="self" type="application/atom+xml" />
-  ${feed.hubs?.map(hub => `<link href="${hub}" rel="hub"/>`).join('\n') ?? ''}
+  <link href="${site.protocol + site.domain}/atom.xml" rel="self" type="application/atom+xml" />${
+  feed.hubs?.map(hub => `\n  <link href="${hub}" rel="hub"/>`).join('') ?? ''
+}
   <updated>${new Date().toJSON()}</updated>
   <author>
     <name><![CDATA[${site.author.name}]]></name>
-  </author>
-  ${genTags(posts)
-    .map(tag => `<category term="${tag}" scheme="${site.protocol + site.domain}/?tags=${encodeURI(tag)}" />`)
-    .join('\n')}
-  ${posts
-    .map(
-      post => `<entry>
+  </author>${genTags(posts)
+    .map(tag => `\n  <category term="${tag}" scheme="${site.protocol + site.domain}/?tags=${encodeURI(tag)}" />`)
+    .join('')}${posts
+  .map(
+    post => `\n  <entry>
     <title type="html"><![CDATA[${post.title}]]></title>
     <link href="${site.protocol + site.domain + post.path}" />
     <id>${site.protocol + site.domain + post.path}</id>
     <published>${new Date(post.published ?? post.created).toJSON()}</published>
-    <updated>${new Date(post.updated ?? post.published ?? post.created).toJSON()}</updated>
-    ${post.layout === 'article' && post.descr ? `<summary type="html"><![CDATA[${post.descr.toString()}]]></summary>` : ''}
+    <updated>${new Date(post.updated ?? post.published ?? post.created).toJSON()}</updated>${
+      post.layout === 'article' && post.descr ? `\n    <summary type="html"><![CDATA[${post.descr.toString()}]]></summary>` : ''
+    }
     <content type="html">
       <![CDATA[${post.html}]]>
-    </content>
-    ${
-      post.tags &&
-      post.tags
-        .map(tag => `<category term="${tag}" scheme="${site.protocol + site.domain}/?tags=${encodeURI(tag)}" />`)
-        .join('\n')
-    }
+    </content>${post.tags
+      ?.map(tag => `\n    <category term="${tag}" scheme="${site.protocol + site.domain}/?tags=${encodeURI(tag)}" />`)
+      .join('')}
   </entry>`
-    )
-    .join('\n')}
+  )
+  .join('')}
 </feed>`
 
 export const get: RequestHandler = async () => ({
