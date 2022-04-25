7bc3017b771bbcd7b98cb071e2ebac8130d60a35
Stanley Man <stanley.wk.man@outlook.com>
2022-04-25 17:01:20 +0800

    Add more themes from daisyui - make theme-selection scrollable

--- src/lib/config/general.ts
+++ src/lib/config/general.ts
@@ -14,8 +14,16 @@ export const theme: ThemeConfig = [
     text: '🧁 Cupcake'
   },
   {
+    name: 'bumblebee',
+    text: '🐝 Bumblebee'
+  },
+  {
     name: 'emerald',
     text: '✳️ Emerald'
+  },
+  {
+    name: 'corporate',
+    text: '🏢 Corporate'
   },
   {
     name: 'valentine',
@@ -26,16 +34,84 @@ export const theme: ThemeConfig = [
     text: '🌃 Synthwave'
   },
   {
+    name: 'retro',
+    text: '🌇 Retro'
+  },
+  {
+    name: 'cyberpunk',
+    text: '🌐 Cyberpunk'
+  },
+  {
     name: 'halloween',
     text: '🎃 Halloween'
+  },
+  {
+    name: 'garden',
+    text: '🏡 Garden'
+  },
+  {
+    name: 'forest',
+    text: '🌲 Forest'
   },
   {
     name: 'aqua',
     text: '💦 Aqua'
   },
   {
+    name: 'lofi',
+    text: '🎶 Lo-Fi'
+  },
+  {
+    name: 'pastel',
+    text: '🌈 Pastel'
+  },
+  {
+    name: 'fantasy',
+    text: '🐣 Fantasy'
+  },
+  {
+    name: 'wirefream',
+    text: '📱 Wireframe'
+  },
+  {
+    name: 'black',
+    text: '🖤 Black'
+  },
+  {
+    name: 'luxury',
+    text: '💰 Luxury'
+  },
+  {
     name: 'dracula',
     text: '🧛 Dracula'
+  },
+  {
+    name: 'cmyk',
+    text: '🖨️ CMYK'
+  },
+  {
+    name: 'autumn',
+    text: '🍂 Autumn'
+  },
+  {
+    name: 'business',
+    text: '🗄️ Business'
+  },
+  {
+    name: 'acid',
+    text: '🌧️ Acid'
+  },
+  {
+    name: 'lemonade',
+    text: '🍋 Lemonade'
+  },
+  {
+    name: 'night',
+    text: '🌃 Night'
+  },
+  {
+    name: 'coffee',
+    text: '☕ Coffee'
   },
   {
     name: 'winter',
