0742e658283454e69d771d6f52b97f4e511174a7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-04-30 22:34:41 +0800

    fix(hooks): 🔥 remove html-minifier
    fix #14

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,31 +1,7 @@
 import type { Handle } from '@sveltejs/kit'
-import { prerendering } from '$app/env'
-import { minify } from 'html-minifier'
 import { site } from '$lib/config/site'
-
-const minificationOptions = {
-  collapseBooleanAttributes: true,
-  collapseWhitespace: true,
-  conservativeCollapse: true,
-  decodeEntities: true,
-  html5: true,
-  ignoreCustomComments: [/^#/],
-  minifyCSS: true,
-  minifyJS: true,
-  removeAttributeQuotes: true,
-  removeComments: true,
-  removeOptionalTags: true,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
-  sortAttributes: true,
-  sortClassName: true
-}
 
 export const handle: Handle = async ({ event, resolve }) =>
   await resolve(event, {
-    transformPage: ({ html }) =>
-      prerendering
-        ? minify(html.replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`), minificationOptions)
-        : html.replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`)
+    transformPage: ({ html }) => html.replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`)
   })
