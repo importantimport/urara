a1bef441cfe8cc031d8a92abd8627e27142b20f7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-04-30 19:41:35 +0800

    refactor: 🎨 code prettify

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,36 @@ export default /** @type {import('tailwindcss/tailwind-config').TailwindConfig} 
   },
   plugins: [typography, daisyui],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter'
+    ]
   }
 }
