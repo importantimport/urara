ea054dbd3524cf8f732801245281708e2da33deb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-05-07 16:51:20 +0800

    fix(prose/img): 🐛 set default alt

--- src/lib/components/prose/img.svelte
+++ src/lib/components/prose/img.svelte
@@ -2,7 +2,7 @@
   let className = undefined
   export { className as class }
   export let src: string
-  export let alt: string
+  export let alt: string = src
   export let loading: 'eager' | 'lazy' = 'lazy'
   export let decoding: 'async' | 'sync' | 'auto' = 'async'
 </script>
