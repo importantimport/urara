e9b3af60d40319052828d73368fb74891e1285bb
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-05-22 01:18:53 +0800

    fix(sitemap): 🐛 include homepage in stiemap

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -4,6 +4,9 @@ import { genPosts } from '$lib/utils/posts'
 
 const render = async (): Promise<string> => `<?xml version='1.0' encoding='utf-8'?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${site.protocol + site.domain}</loc>
+    </url>
     ${genPosts()
       .map(
         post => `
