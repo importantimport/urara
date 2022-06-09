c14d6bbbb34d3f0de795b871a00630d3a75e63ac
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-06-09 16:57:04 +0800

    refactor(manifest): 🔥 remove manifest.ts

--- src/routes/manifest.webmanifest.ts
+++ src/routes/manifest.webmanifest.ts
@@ -1,10 +1,32 @@
 import type { RequestHandler } from '@sveltejs/kit'
-import { manifest } from '$lib/config/manifest'
-import { toSnake } from '$lib/utils/case'
+import { site } from '$lib/config/site'
+import { any, maskable } from '$lib/config/icon'
 
 export const get: RequestHandler = () => ({
   headers: {
     'Content-Type': 'application/manifest+json; charset=utf-8'
   },
-  body: JSON.stringify(Object.fromEntries(Object.entries(manifest).map(([k, v]) => [toSnake(k), v])), null, 4)
+  body: JSON.stringify(
+    {
+      name: site.title,
+      short_name: site.title,
+      lang: site.lang,
+      description: site.description,
+      id: site.protocol + site.domain + '/',
+      start_url: '/',
+      scope: '/',
+      display: 'standalone',
+      orientation: 'portrait',
+      background_color: site.themeColor,
+      theme_color: site.themeColor,
+      icons: [
+        ...Object.values(any)
+          .filter(icon => icon.sizes !== '180x180')
+          .map(icon => ({ ...icon, purpose: 'any' })),
+        ...Object.values(maskable).map(icon => ({ ...icon, purpose: 'maskable' }))
+      ]
+    },
+    null,
+    2
+  )
 })
