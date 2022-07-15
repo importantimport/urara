cc70b860220c549c660f210b31d654ec0cb6c277
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-07-15 22:48:59 +0800

    feat: ✨ experimental actions support

--- src/lib/components/layouts/_post.svelte
+++ src/lib/components/layouts/_post.svelte
@@ -6,6 +6,7 @@
   import { post as postConfig } from '$lib/config/post'
   import Status from '$lib/components/post_status.svelte'
   import Pagination from '$lib/components/post_pagination.svelte'
+  import Action from '$lib/components/post_action.svelte'
   import Comment from '$lib/components/post_comment.svelte'
   import Footer from '$lib/components/footer.svelte'
 
@@ -32,7 +33,9 @@
     in:fly={{ x: 25, duration: 300, delay: 500 }}
     out:fly={{ x: 25, duration: 300 }}
     class="flex-1 w-full max-w-screen-md order-first ease-out transform mx-auto xl:mr-0">
-    <slot name="left" />
+    {#if browser}
+      <Action {post} />
+    {/if}
   </div>
   <div
     in:fly={{ x: -25, duration: 300, delay: 500 }}
---
+++ src/lib/components/post_action.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let post: Urara.Post
+  const actions = import.meta.globEager('/src/lib/components/actions/*.svelte')
+</script>
+  
+<div class="sticky top-24 flex flex-col gap-4 w-fit h-[calc(100vh-12rem)] ml-auto mr-8 my-8 justify-center">
+  {#if Object.keys(actions).length}
+    {#each Object.values(actions) as action}
+      <svelte:component this={action.default} {post} />
+    {/each}
+  {/if}
+</div>
