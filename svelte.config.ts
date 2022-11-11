ba389fba103cfae1cb0119952633289cb27153b2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-11 15:54:53 +0800

    fix(svelte): 🚑️ warn handleMissingId

--- svelte.config.ts
+++ svelte.config.ts
@@ -24,6 +24,9 @@ export default defineConfig({
           assets: 'build',
           fallback: undefined
         }),
+    prerender: {
+      handleMissingId: 'warn'
+    },
     csp: { mode: 'auto' }
   }
 })
