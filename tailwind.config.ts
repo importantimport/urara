1f8e884275b078410cd0b4cd0c048357f18c2d43
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-26 18:02:50 +0800

    refactor(tailwind/daisyui): ♻️ use config theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { theme } from './src/lib/config/general'
 // @ts-ignore Could not find a declaration file for module '@tailwindcss/typography'.
 import typography from '@tailwindcss/typography'
 // @ts-ignore Could not find a declaration file for module 'daisyui'.
@@ -5,41 +6,7 @@ import daisyui from 'daisyui'
 
 export default {
   content: ['./src/**/*.{html,md,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
+  theme: { extend: {} },
   plugins: [typography, daisyui],
-  daisyui: {
-    themes: [
-      'light',
-      'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter'
-    ]
-  }
+  daisyui: { themes: theme.map(({ name }) => name) }
 }
