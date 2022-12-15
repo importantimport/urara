fcd2a347cb18d08314b85f15db2b105ea998d076
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-15 18:15:40 +0800

    refactor!: 💥 always use trailing slash

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,6 @@
 import type { LayoutLoad } from './$types'
 export const prerender = true
+export const trailingSlash = 'always'
 export const load: LayoutLoad = async ({ url, fetch }) => ({
   path: url.pathname,
   res: await fetch('/posts.json').then(res => res.json())
