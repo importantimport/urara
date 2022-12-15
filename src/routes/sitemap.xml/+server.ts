33fc5d97bda4345061f63b0f258c1af4244f9a85
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-15 18:29:00 +0800

    refactor(sitemap): ✨ update urlset

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -2,8 +2,15 @@ import type { RequestHandler } from './$types'
 import { site } from '$lib/config/site'
 import { genPosts } from '$lib/utils/posts'
 
-const render = (): string => `<?xml version='1.0' encoding='utf-8'?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+const render = (): string =>
+  `<?xml version='1.0' encoding='utf-8'?>
+  <urlset
+    xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xhtml="https://www.w3.org/1999/xhtml"
+    xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+    xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+    xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+    xmlns:video="https://www.google.com/schemas/sitemap-video/1.1">
     <url>
       <loc>${site.protocol + site.domain}</loc>
     </url>
