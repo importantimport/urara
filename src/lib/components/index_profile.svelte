cf296f8d0b6ebd389ef8eafd4f98546246f87860
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-15 21:10:56 +0800

    pref(index_profile): ⚡️ optimized with imagetools

--- src/lib/components/index_profile.svelte
+++ src/lib/components/index_profile.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
   import { site } from '$lib/config/site'
+  import { src, width, height } from '/static/assets/maskable@512.png?width=384&format=webp&metadata'
+  import srcset from '/static/assets/maskable@512.png?w=48;96;192&avif&srcset'
 </script>
 
 <div
   class="h-card flex flex-col gap-4 sticky top-24 card card-body p-4 items-right xl:border-2 xl:py-8 border-base-content/10 xl:ml-auto xl:mr-8 xl:max-w-xs">
   <a href={site.protocol + site.domain} class="hidden u-url u-uid">{site.author.name}</a>
   <figure class="relative mx-auto group">
-    <img
-      class="u-photo rounded-full shadow-xl hover:shadow-2xl transition-shadow z-10 w-24 h-24 md:w-32 md:h-32"
-      src={site.author.avatar}
-      alt={site.author.name} />
+    <picture>
+      <source {srcset} type="image/avif" />
+      <img
+        class="u-photo rounded-full shadow-xl hover:shadow-2xl transition-shadow z-10 w-24 h-24 md:w-32 md:h-32"
+        {src}
+        {width}
+        {height}
+        alt={site.author.name} />
+    </picture>
     {#if site.author.status}
       <div
         class="absolute z-20 rounded-full w-8 h-8 md:w-10 md:h-10 bottom-0 right-0 bg-base-100 shadow-xl text-lg md:text-xl text-center py-0.5 md:py-1.5">
