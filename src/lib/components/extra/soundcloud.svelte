0d665754797ff805ec0ad156666c01371d00f1f2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-08 22:32:20 +0800

    feat(component/extra): ✨ add soundcloud

---
+++ src/lib/components/extra/soundcloud.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let type: 'track' | 'playlist' = 'track'
+  export let id: string
+  export let visual: boolean = true
+  export let color: string = '#ff5500'
+  export let autoplay: booelan = false
+  export let width: string = '100%'
+  export let height: string = visual ? '300' : type === 'tracks' ? '166' : '450'
+  const src = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/${type}s/${id}?${new URLSearchParams({
+    color,
+    visual,
+    auto_play: autoplay,
+    hide_related: 'false',
+    show_comments: 'true',
+    show_user: 'true',
+    show_reposts: 'false',
+    show_teaser: 'true'
+  }).toString()}`
+</script>
+
+<iframe title="SoundCloud music player" {width} {height} scrolling="no" frameborder="no" allow="autoplay" {src} />
