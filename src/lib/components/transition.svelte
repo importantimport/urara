18836f17e3b992e74516605154637f6a886def2b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-13 14:48:17 +0800

    refactor(layout): ♻️ split transition component

---
+++ src/lib/components/transition.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { fly } from 'svelte/transition'
+  export let path: string = ''
+</script>
+
+{#key path}
+  <div
+    class="bg-base-100 md:bg-base-200 min-h-screen pt-16 md:pb-8 lg:pb-16"
+    in:fly={{ y: 100, duration: 300, delay: 300 }}
+    out:fly={{ y: -100, duration: 300 }}>
+    <slot />
+  </div>
+{/key}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,12 @@
   import type { LayoutData } from './$types'
   import { onMount } from 'svelte'
   import { browser, dev } from '$app/environment'
-  import { fly } from 'svelte/transition'
   import { genTags } from '$lib/utils/posts'
   import { posts, tags } from '$lib/stores/posts'
   import { registerSW } from 'virtual:pwa-register'
   import Head from '$lib/components/head_static.svelte'
   import Header from '$lib/components/header.svelte'
+  import Transition from '$lib/components/transition.svelte'
   import 'uno.css'
   import '../app.pcss'
 
@@ -33,11 +33,6 @@
 
 <Header {path} />
 
-{#key data.path}
-  <div
-    class="bg-base-100 md:bg-base-200 min-h-screen pt-16 md:pb-8 lg:pb-16"
-    in:fly={{ y: 100, duration: 300, delay: 300 }}
-    out:fly={{ y: -100, duration: 300 }}>
-    <slot />
-  </div>
-{/key}
+<Transition {path}>
+  <slot />
+</Transition>
