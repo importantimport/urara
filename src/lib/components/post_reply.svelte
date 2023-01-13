5aee6ac645f570205755a85b118abb92ca5d825a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-13 14:15:01 +0800

    refactor: 👽️ remove array in reply to

--- src/lib/components/post_reply.svelte
+++ src/lib/components/post_reply.svelte
@@ -6,25 +6,12 @@
 
 <div class="flex flex-wrap gap-2 rounded-box outline outline-neutral/10 p-4 {className}">
   <span class="flex-none font-bold uppercase opacity-30">Reply to:&nbsp;</span>
-  {#if Array.isArray(in_reply_to)}
-    {#each in_reply_to as reply}
-      <a
-        href={reply}
-        rel="noopener external"
-        target="_blank"
-        class="flex-none flex rounded-badge bg-base-200 hover:bg-base-300 transition-all gap-2 px-4 u-in-reply-to">
-        <span class="i-heroicons-outline-reply my-auto !w-4 !h-4" />
-        {reply}
-      </a>
-    {/each}
-  {:else}
-    <a
-      href={in_reply_to}
-      rel="noopener external"
-      target="_blank"
-      class="ml-auto flex-none flex rounded-badge bg-base-200 hover:bg-base-300 transition-all gap-2 px-4 u-in-reply-to">
-      <span class="i-heroicons-outline-reply my-auto !w-4 !h-4" />
-      {in_reply_to}
-    </a>
-  {/if}
+  <a
+    href={in_reply_to}
+    rel="noopener external"
+    target="_blank"
+    class="ml-auto flex-none flex rounded-badge bg-base-200 hover:bg-base-300 transition-all gap-2 px-4 u-in-reply-to">
+    <span class="i-heroicons-outline-reply my-auto !w-4 !h-4" />
+    {in_reply_to}
+  </a>
 </div>
