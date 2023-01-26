89d8512758073904372e86ae2475713d23893083
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-26 20:50:11 +0800

    refactor(config/site): ♻️ more reasonable preset

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
-  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? 'https://',
+  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
   title: 'Urara',
   subtitle: 'Sweet & Powerful SvelteKit Blog Template',
