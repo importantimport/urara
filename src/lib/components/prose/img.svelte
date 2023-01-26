f56af7f13e6d99818b4509ce3d1f81c456dbc300
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-26 20:49:00 +0800

    fix(prose/img): 🐛 source check (#56)
    closed #56

--- src/lib/components/prose/img.svelte
+++ src/lib/components/prose/img.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   /* @see {@link https://github.com/sveltejs/kit/issues/241#issuecomment-1363621896} */
 
-  const images = import.meta.glob(['/static/**/*.{jpg,jpeg,png,webp,avif}', '!/static/assets'], {
-    import: 'default',
-    eager: true
-  })
+  type Image = {
+    src: string
+    w: number
+    h: number
+  }
 
-  const sources = import.meta.glob(['/static/**/*.{jpg,jpeg,png,webp,avif}', '!/static/assets'], {
+  const sources = import.meta.glob<Image[]>(['/static/**/*.{jpg,jpeg,png,webp,avif}', '!/static/assets'], {
     query: {
       format: 'avif',
       quality: '80',
@@ -23,18 +24,14 @@
   export let alt: string = src
   export let loading: 'eager' | 'lazy' = 'lazy'
   export let decoding: 'async' | 'sync' | 'auto' = 'async'
-
-  type Image = {
-    src: string
-    w: number
-    h: number
-  }
-
-  const image: Image = images[`/static${src}`] as any
-  const source: Image[] = sources[`/static${src}`] as any
+  let source: Image[] | undefined = sources[`/static${src}`]
 </script>
 
-<picture>
-  <source srcset={source.map(({ src, w }) => `${src} ${w}w`).join(', ')} type="image/avif" />
-  <img src={image.src} {alt} class={className ?? 'rounded-lg my-2'} {loading} {decoding} />
-</picture>
+{#if source}
+  <picture>
+    <source srcset={source.map(({ src, w }) => `${src} ${w}w`).join(', ')} type="image/avif" />
+    <img {src} {alt} class={className ?? 'rounded-lg my-2'} {loading} {decoding} />
+  </picture>
+{:else}
+  <img {src} {alt} class={className ?? 'rounded-lg my-2'} {loading} {decoding} />
+{/if}
