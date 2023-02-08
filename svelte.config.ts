66636b828bc82fbf6c214d19294891064938185f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-02-08 13:29:47 +0800

    chore: ➖ remove `svelte-preprocess-import-assets`

--- svelte.config.ts
+++ svelte.config.ts
@@ -7,14 +7,13 @@ import adapterStatic from '@sveltejs/adapter-static'
 // svelte preprocessor
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-import importAssets from 'svelte-preprocess-import-assets'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
 const defineConfig = (config: Config) => config
 
 export default defineConfig({
   extensions: ['.svelte', ...(mdsvexConfig.extensions as string[])],
-  preprocess: [mdsvex(mdsvexConfig), importAssets(), vitePreprocess()],
+  preprocess: [mdsvex(mdsvexConfig), vitePreprocess()],
   kit: {
     adapter: Object.keys(process.env).some(key => ['VERCEL', 'NETLIFY'].includes(key))
       ? adapterAuto()
