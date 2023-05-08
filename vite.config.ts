3f449443bed043a60ef5a9ed8fa9fa9b8c8ba487
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-08 14:31:44 +0800

    fix(unocss): ➕ add `@unocss/extractor-svelte`

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@
 import { defineConfig } from 'vite'
 // vite plugin
 import UnoCSS from 'unocss/vite'
-import { presetTagify, presetIcons, extractorSvelte } from 'unocss'
+import { presetTagify, presetIcons } from 'unocss'
+import extractorSvelte from '@unocss/extractor-svelte'
 import { imagetools } from 'vite-imagetools'
 import { sveltekit as SvelteKit } from '@sveltejs/kit/vite'
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
