417b5d0f7ddf641661b2cd730d4612c0e8809423
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-14 22:18:39 +0800

    feat: ✨ support for specifying adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,33 @@
 // svelte adapter
-import adapterVercel from '@sveltejs/adapter-vercel'
-import adapterNetlify from '@sveltejs/adapter-netlify'
+import adapterAuto from '@sveltejs/adapter-auto'
+import adapterNode from '@sveltejs/adapter-node'
 import adapterStatic from '@sveltejs/adapter-static'
 // svelte preprocessor
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
+const adapter = {
+  auto: adapterAuto(),
+  node: adapterNode(),
+  static: adapterStatic({
+    pages: 'build',
+    assets: 'build',
+    fallback: undefined
+  })
+}
+
 /** @type {import("@svletejs/kit".Config)} */
 export default {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
   preprocess: [mdsvex(mdsvexConfig), vitePreprocess()],
   kit: {
-    adapter: Object.keys(process.env).some(key => key === 'VERCEL')
-      ? adapterVercel()
-      : Object.keys(process.env).some(key => key === 'NETLIFY')
-      ? adapterNetlify()
-      : adapterStatic({
-          pages: 'build',
-          assets: 'build',
-          fallback: undefined
-        }),
+    adapter: 
+      process.env.ADAPTER
+        ? adapter[process.env.ADAPTER.toLowerCase()]
+        : Object.keys(process.env).some(key => ['VERCEL', 'CF_PAGES', 'NETLIFY', 'GITHUB_ACTION_REPOSITORY', 'SST'].includes(key))
+          ? adapter['auto']
+          : adapter['static'],
     prerender: {
       handleMissingId: 'warn'
     },
